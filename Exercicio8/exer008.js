cd8d7cc1efc910bb6bed4924534ada711911f3da
Marcos Vinicius Bastos da Silva <bastosmarcos90@gmail.com>
2022-08-15 20:29:16 -0300

    update

---
+++ Exercicio8/exer008.js
@@ -0,0 +1,8 @@
+var vel = 60
+console.log(`A velocida do seu carro é de ${vel} kilometros por H`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade limite.Multado!`)
+} 
+else {
+    console.log(`Dirija sempre utilizando o cinto de segurança`)
+}
