f295c9ee6d430983165529eb75e0a09ec6d11cb1
Marcos Vinicius Bastos da Silva <bastosmarcos90@gmail.com>
2022-09-05 18:33:40 -0300

    update

--- Exercicio1.5/index.js
+++ Exercicio1.5/index.js
@@ -1,4 +1,51 @@
 document.getElementById("texto").innerHTML = "Meu primeiro texto JS"
 console.log('Isso é um console.log')
+/*
+// let nao permite a declaração de outra variavel. LET É UM SÓ dentro de um escopo
+//funcao de soma
+function soma(valor1,valor2){
+    return valor1 + valor2
+}
+// funcao de cotação do dolar
+function RealParaDolar(real, cotacaoDoDolar){
+    return real * cotacaoDoDolar;
+}
 
-// let nao permite a declaração de outra variavel. LET É UM SÓ
+var valorEmReal = 50.07
+var conversaoDolar = 5.15
+
+
+let total = RealParaDolar(valorEmReal,conversaoDolar);
+alert("O valor em real é R$:"+valorEmReal+" o valor em dolar é  U$ é: "+total)
+
+
+function HelloPessoal(){
+    alert("Olá Pessoal!")
+}
+
+function paraCelsius(valorFahrenheit){
+    return (5/9) * (valorFahrenheit - 32);
+}
+
+var celsius = paraCelsius(80);
+
+alert("A temperatura é de  " + celsius +"graus cesius")
+*/
+
+//metodo é uma propriedade quando tem uma função.   Objeto,propriedade função.
+const carro = {
+    marca:"ford",
+    modelo:"ka",
+    ano:"2015",
+    placa:"ABC1234",
+    buzina: function() {alert('BIIIIIIIIIIIIIIIIIII')},
+    completo: function(){
+        return "A marca é " +this.marca + "e o modelo é" +this.modelo
+    }
+};
+
+console.log(carro.completo())
+
+function eventoClick(){
+    alert("Acionou um evento de click")
+}
