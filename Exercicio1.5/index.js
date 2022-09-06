1e638b27fddbb8da4783f079218192da29c7fed7
Marcos Vinicius Bastos da Silva <bastosmarcos90@gmail.com>
2022-09-06 13:53:14 -0300

    update

--- Exercicio1.5/index.js
+++ Exercicio1.5/index.js
@@ -70,3 +70,4 @@ function teclaPressionada(){
     console.log(input)
     
 }
+
---
+++ Exercicio16/index.js
@@ -0,0 +1,6 @@
+const lista1 = ["arroz", "feijao","leite","macarrão"]
+const lista2 = ["Suco", "refrigerante", "carne"]
+const lista3 = ["fandangos", "cheetos","lays"]
+const superLista = lista1.concat(lista2,lista3)
+
+document.getElementById("teste").innerHTML =superLista;
