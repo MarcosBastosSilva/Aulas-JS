90f1abfabad4dfb4fd635bd9077cbf883dfb623b
Marcos Vinicius Bastos da Silva <bastosmarcos90@gmail.com>
2022-09-09 20:35:45 -0300

    update

---
+++ Exercicio22/index.js
@@ -0,0 +1,14 @@
+class Carro {
+    constructor(valor1,valor2,valor3){
+        this.marca = valor1
+        this.modelo = valor2
+        this.ano = valor3
+    }
+    buzina(){
+        return  this.modelo +"buzinou : biiiiiiiiiii"
+    }
+}
+
+const uno = new Carro("fiat", "uno", 2001)
+const gol = new Carro("Volkswagem", "gol",2007)
+console.log(uno)
